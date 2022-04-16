e713d2374fa5e07214d9f1d3f4ea901bc0345e2d
ffadelm <faysalfadelm0107@gmail.com>
2022-04-16 17:51:35 +0700

    Factory & Faker

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
 @section('container')
 <article>
     <h2>{{$post->title}}</h2>
-    <p>by: <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}/" class="text-decoration-none">{{ $post->category->name }}</a></p>
+    <p>by: <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}/" class="text-decoration-none">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
 </article>
 
