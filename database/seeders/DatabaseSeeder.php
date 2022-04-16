f251494b2d62b522fe61f1e04941f23dc42a91e8
ffadelm <faysalfadelm0107@gmail.com>
2022-04-16 17:05:24 +0700

    Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +18,59 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Faysal Fadel Maulana',
+            'email' => 'faysal@gmail.com',
+            'password' => bcrypt('123456'),
+        ]);
+
+        User::create([
+            "name" => "Jacki",
+            "email" => "jack@mail.com",
+            "password" => bcrypt("123456"),
+        ]);
+
+        Category::create([
+            'name' => 'PHP Language',
+            'slug' => 'php-lang',
+        ]);
+
+        Category::create([
+            'name' => 'Laravel',
+            'slug' => 'laravel',
+        ]);
+
+        Category::create([
+            'name' => 'JavaScript',
+            'slug' => 'javascript',
+        ]);
+
+        Post::create([
+            'title' => 'Laravel 5.7',
+            'slug' => 'laravel-5.7',
+            'excerpt' => 'Laravel 5.7 is a free, open-source PHP web framework, created by Taylor Otwell and intended for...',
+            'body' => 'Laravel 5.7 is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller (MVC) architectural pattern. It is based on Symfony, and its components are interoperable with the Symfony 2 framework.',
+            'category_id' => 2,
+            'user_id' => 1,
+        ]);
+
+        Post::create([
+            'title' => 'vue js',
+            'slug' => 'vue-js',
+            'excerpt' => 'Vue.js is a progressive framework for building user interfaces. It is a library for building UI components based on reactive data...',
+            'body' => '<p>Vue.js is a progressive framework for building user interfaces. It is a library for building UI components based on reactive data. Vue.js is designed from the ground up to be incrementally adoptable. It is also designed to be efficient and performant, as it is idiomatically designed for this purpose. Vue.js is a library for building UI components based on reactive data. Vue.js is designed from the ground up to be incrementally adoptable.</p> <p>It is also designed to be efficient and performant, as it is idiomatically designed for this purpose. Vue.js is a library for building UI components based on reactive data. Vue.js is designed from the ground up to be incrementally adoptable. It is also designed to be efficient and performant, as it is idiomatically designed for this purpose. Vue.js is a library for building UI components based on reactive data. Vue.js is designed from the ground up to be incrementally adoptable. It is also designed to be efficient and performant, as it is idiomatically designed for this purpose. Vue.js is a library for building UI components based on reactive data. Vue.js is designed from the ground up to be incrementally adoptable.</p> <p>It is also designed to be efficient and performant, as it is idiomatically designed for this purpose. Vue.js is a library for building UI components based on reactive data. Vue.js is designed from the ground up to be incrementally adoptable. It is also designed to be efficient and performant, as it is idiomatically designed for this purpose. Vue.js is a library for building UI components based on reactive data. Vue.js is designed from the ground up to be incrementally adoptable. It is also designed to be efficient and performant, as it is idiomatically designed for this purpose.</p><p> Vue.js is a library for building UI components based on reactive data. Vue.js is designed from the ground up to be incrementally adoptable. It is also designed to be efficient and performant, as it is idiomatically designed for this purpose. Vue.js is a library for building UI components based on reactive data. Vue.js is designed from the ground up to be incrementally adoptable. It is also designed to be efficient and performant, as it is id</p>',
+            'category_id' => 3,
+            'user_id' => 1,
+        ]);
+
+        Post::create([
+            "title" => "PHP 7",
+            "slug" => "php-7",
+            "excerpt" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, iure beatae. Iste numquam quisquam voluptas sunt non tenetur error soluta harum. Nostrum velit voluptate fuga sed doloribus, quas eveniet dignissimos.",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum hic dolore ipsa laboriosam accusantium rerum expedita recusandae aperiam architecto facere vel est molestiae libero fugit deleniti facilis totam id repudiandae doloremque consequuntur tempora perferendis autem, officiis exercitationem. Asperiores beatae, exercitationem eaque repellat officia dolor ea nulla quis commodi unde magnam sapiente delectus necessitatibus quas, laudantium aspernatur, adipisci vitae iusto hic deserunt? Nostrum molestias neque iste magnam dolores, ratione alias eius amet iusto distinctio magni molestiae voluptates, explicabo consequatur quasi error quas, praesentium aspernatur natus? Eum expedita, eaque molestias earum exercitationem tenetur est libero perferendis harum ratione animi esse, maxime sed, officia pariatur at. Illum error nesciunt cupiditate alias. Error ea voluptatem hic nemo voluptatibus laborum sint, quo nesciunt omnis? Omnis, aliquam modi? Vero corrupti error deserunt, architecto optio excepturi eos quo, quibusdam dicta sed voluptates ad laudantium officia ducimus obcaecati fugit debitis. Porro saepe minima sed minus culpa? Quod, nemo.",
+            "category_id" => 1,
+            "user_id" => 2,
+        ]);
     }
 }
